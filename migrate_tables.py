36e27342d6266e8578c64257d91cee025e72ec8e
Sam Quinn <samquinn20@gmail.com>
2022-02-15 21:10:02 +0000

    remove old import

--- migrate_tables.py
+++ migrate_tables.py
@@ -2,7 +2,6 @@ from sqlalchemy_utils import drop_database
 
 from app import config
 from app.database.manage import init_database
-from app.internal.database import engine
 
 drop_database(str(config.SQLALCHEMY_DATABASE_URI))
 
