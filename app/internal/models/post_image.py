59f6dd2c7b489745d128d4e4eb201fdb565d317a
Sam Quinn <samquinn20@gmail.com>
2022-02-15 21:03:46 +0000

    delete internal

--- app/internal/models/post_image.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
-
-
-# class PostImage(Record):
-#    __tablename__ = "post_image"
-#
-#    post_id = Column(Integer, ForeignKey("post.id"))
-#    image_url = Column(String, nullable=False)
