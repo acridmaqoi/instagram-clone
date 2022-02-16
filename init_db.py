185b8f14a5488a6e0b97bbbd615e6aa582573214
Sam Quinn <samquinn20@gmail.com>
2022-02-16 13:39:52 +0000

    move scripts into root

--- init_db.py
+++ init_db.py
@@ -3,6 +3,9 @@ from sqlalchemy_utils import drop_database
 from app import config
 from app.database.manage import init_database
 
-drop_database(str(config.SQLALCHEMY_DATABASE_URI))
+try:
+    drop_database(str(config.SQLALCHEMY_DATABASE_URI))
+except:
+    pass
 
 init_database()
