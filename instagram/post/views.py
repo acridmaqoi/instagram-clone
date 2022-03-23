9f6fa75761f45f7dfc79ee0048bdd3d5a5f2cec3
Sam Quinn <samquinn20@gmail.com>
2022-03-23 17:01:05 +0000

    delete like comment from post service

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -1,7 +1,7 @@
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 from instagram.auth.models import InstagramUser
 from instagram.auth.service import get_current_user
 from instagram.database.core import get_db
-from fastapi import APIRouter, Body, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from .models import Comment, Post, PostCreate, PostRead
@@ -29,20 +29,6 @@ def get_current_post(post_id: int, db: Session = Depends(get_db)):
     return post
 
 
-def get_current_comment(
-    comment_id: int,
-    current_post: Post = Depends(get_current_post),
-    db: Session = Depends(get_db),
-):
-    comment = get_comment(db=db, current_post=current_post, comment_id=comment_id)
-    if not comment:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Comment with id={comment_id} not found",
-        )
-    return comment
-
-
 @router.post("", response_model=PostRead)
 def create_post(
     post_in: PostCreate,
@@ -67,76 +53,3 @@ def delete_post(
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
     delete(db=db, post_id=current_post.id)
-
-
-@router.post("/{post_id}/comments")
-def comment(
-    comment: str = Body(..., embed=True),
-    current_post: Post = Depends(get_current_post),
-    current_user: InstagramUser = Depends(get_current_user),
-    db: Session = Depends(get_db),
-):
-    add_comment(
-        db=db, current_post=current_post, comment=comment, current_user=current_user
-    )
-
-
-@router.delete("/{post_id}/comments/{comment_id}")
-def delete_post_comment(
-    comment_id: int,
-    current_post: Post = Depends(get_current_post),
-    current_user: InstagramUser = Depends(get_current_user),
-    db: Session = Depends(get_db),
-):
-    comment = get_comment(db=db, current_post=current_post, comment_id=comment_id)
-    if comment is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Comment with id={comment_id} not found",
-        )
-    if comment.user_id != current_user.id:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED)
-
-    uncomment(db=db, current_post=current_post, comment_id=comment_id)
-
-
-@router.post("/{post_id}/likes")
-def like_post(
-    current_post: Post = Depends(get_current_post),
-    current_user: InstagramUser = Depends(get_current_user),
-    db: Session = Depends(get_db),
-):
-    like_post_or_comment(db=db, current_user=current_user, current_entity=current_post)
-
-
-@router.delete("/{post_id}/likes")
-def dislike_post(
-    current_post: Post = Depends(get_current_post),
-    current_user: InstagramUser = Depends(get_current_user),
-    db: Session = Depends(get_db),
-):
-    dislike_post_or_comment(
-        db=db, current_entity=current_post, current_user=current_user
-    )
-
-
-@router.post("/{post_id}/comments/{comment_id}/likes")
-def like_comment(
-    current_comment: Comment = Depends(get_current_comment),
-    current_user: InstagramUser = Depends(get_current_user),
-    db: Session = Depends(get_db),
-):
-    like_post_or_comment(
-        db=db, current_entity=current_comment, current_user=current_user
-    )
-
-
-@router.delete("/{post_id}/comments/{comment_id}/likes")
-def dislike_comment(
-    current_comment: Comment = Depends(get_current_comment),
-    current_user: InstagramUser = Depends(get_current_user),
-    db: Session = Depends(get_db),
-):
-    dislike_post_or_comment(
-        db=db, current_entity=current_comment, current_user=current_user
-    )
