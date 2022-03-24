1df18f75853e57424a6b8b1beee09bacf4a02e14
Sam Quinn <samquinn20@gmail.com>
2022-03-24 00:06:59 +0000

    upload image to s3

--- instagram/config.py
+++ instagram/config.py
@@ -6,3 +6,4 @@ SQLALCHEMY_DATABASE_URI = config(
     "SQLALCHEMY_DATABASE_URI",
     default="postgresql://postgres:password@localhost:5433/instagram",
 )
+S3_BUCKET_NAME = config("S3_BUCKET_NAME", default="instagram-static-123481")
--- instagram/static/instagram/src/Upload.js
+++ instagram/static/instagram/src/Upload.js
@@ -1,8 +1,28 @@
 import React, { useEffect, useState } from "react";
+import axios from "./axios";
 
 function Upload() {
   const [images, setImages] = useState([]);
   const [imageURLs, setImageURLs] = useState([]);
+
+  const uploadImage = async () => {
+    const url = await axios.get("/utils/s3url").then((res) => res.data.url);
+
+    var bodyFormData = new FormData();
+    bodyFormData.append("image", images[0]);
+
+    await fetch(url, {
+      method: "PUT",
+      body: images[0],
+    });
+
+    const imageUrl = url.split("?")[0];
+    console.log(imageUrl);
+
+    const img = document.createElement("img");
+    img.src = imageUrl;
+    document.body.appendChild(img);
+  };
 
   useEffect(() => {
     if (images.length < 1) return;
@@ -21,6 +41,8 @@ function Upload() {
       {imageURLs.map((imageSrc) => (
         <img src={imageSrc} />
       ))}
+
+      {images.length > 0 && <button onClick={uploadImage}>upload</button>}
     </>
   );
 }
