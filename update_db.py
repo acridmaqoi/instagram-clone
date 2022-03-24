2f6bff65e5e3ea09007b0434674fbeafa0d50189
Sam Quinn <samquinn20@gmail.com>
2022-03-24 14:06:18 +0000

    `create_post` endpoint accepts image urls

--- update_db.py
+++ update_db.py
@@ -1,8 +1,9 @@
 from sqlalchemy_utils import drop_database
 
 from instagram import config
-from instagram.database.manage import update_database
+from instagram.database.manage import init_database, update_database
 
 drop_database(str(config.SQLALCHEMY_DATABASE_URI))
 
-update_database()
+# update_database()
+init_database()
