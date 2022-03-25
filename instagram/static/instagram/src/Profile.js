5c93378832928abd499ee9254e399bad0dcab0e9
Sam Quinn <samquinn20@gmail.com>
2022-03-25 16:50:43 +0000

    style profile page

--- instagram/static/instagram/src/Profile.js
+++ instagram/static/instagram/src/Profile.js
@@ -1,6 +1,8 @@
+import Avatar from "@mui/material/Avatar";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "./axios";
+import "./Profile.css";
 
 function Profile() {
   const navigate = useNavigate();
@@ -16,35 +18,51 @@ function Profile() {
       setUser(_user);
 
       axios.get(`/posts?user_id=${_user.id}`).then((res) => {
-        console.log(res.data);
-        setUserPosts(res.data);
+        console.log(res.data.posts);
+        setUserPosts(res.data.posts);
       });
     });
   }, []);
 
   return (
     <div className="profile">
-      <div className="profile__container">
-        <div className="profile__lineOne">{user?.username}</div>
-
-        <div className="profile__lineTwo">
-          <div className="profile__sectionOne">{user?.post_count} Posts</div>
-          <div className="profile__sectionTwo">
-            {user?.follower_count} Followers
+      <div className="profile__header">
+        <div className="profile__headerImage">
+          <Avatar sx={{ height: "100px", width: "100px" }} />
+        </div>
+        <div className="profile__headerInfo">
+          <div className="profile__title">{user?.username}</div>
+          <div className="profile__stats">
+            <div className="profile__stat">
+              <span className="profile__statNumber">{user?.post_count}</span>{" "}
+              Posts
+            </div>
+            <div className="profile__stat">
+              <span className="profile__statNumber">
+                {user?.follower_count}
+              </span>{" "}
+              Followers
+            </div>
+            <div className="profile__stat">
+              <span className="profile__statNumber">{user?.follow_count}</span>{" "}
+              Following
+            </div>
           </div>
-          <div className="profile__sectionTwo">
-            {user?.follow_count} Following
+          <div className="profile__desc">
+            <div className="profile__name">{user?.name}</div>
           </div>
         </div>
-        <div className="profile__lineThree">{user?.name}</div>
       </div>
 
-      <div className="posts__container">
-        {userPosts?.posts.map((post) => (
-          <img
-            onClick={(e) => navigate(`/p/${post.id}`)}
-            src={post.images[0].url}
-          />
+      <div className="profile__postsContainer">
+        {userPosts?.map((post, index) => (
+          <div className="profile__postItem">
+            <img
+              className="profile__postItem"
+              onClick={(e) => navigate(`/p/${post.id}`)}
+              src={post.images[0].url}
+            />
+          </div>
         ))}
       </div>
     </div>
