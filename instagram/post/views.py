a5ba751d05e83c446c75989c4237b51ef8da5a89
Sam Quinn <samquinn20@gmail.com>
2022-04-13 12:34:12 +0100

    fix all_posts bug

--- instagram/post/views.py
+++ instagram/post/views.py
@@ -60,9 +60,7 @@ def get_all_posts_for_user(
     authenticated_user: InstagramUser = Depends(get_authenticated_user),
     db: Session = Depends(get_db),
 ):
-    posts = get_all_for_user(
-        user_id=current_user.id, current_user=current_user.id, db=db
-    )
+    posts = get_all_for_user(user_id=current_user.id, current_user=current_user, db=db)
     return {"posts": posts, "count": len(posts)}
 
 
