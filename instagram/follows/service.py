f02b62d2323213d33c45b707b72928a5fc91a2aa
Sam Quinn <samquinn20@gmail.com>
2022-04-14 23:45:49 +0100

    create follow

--- instagram/follows/service.py
+++ instagram/follows/service.py
@@ -1,27 +1,23 @@
 from instagram.user.models import InstagramUser
+from psycopg2.errors import CheckViolation, UniqueViolation
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from .models import Follow
 
 
-def follow(db: Session, user: InstagramUser, current_user: InstagramUser) -> None:
-    if user.id == current_user.id:
-        # user cannot follow themselves
-        return
-
-    # user can only follow once
-    if (
-        not db.query(Follow)
-        .filter(Follow.from_user_id == current_user.id)
-        .filter(Follow.to_user_id == user.id)
-        .one_or_none()
-    ):
-        follow = Follow(from_user_id=current_user.id, to_user_id=user.id)
-        db.add(follow)
+def create(db: Session, from_user: InstagramUser, to_user: InstagramUser) -> None:
+    try:
+        db.add(Follow(from_user_id=from_user.id, to_user_id=to_user.id))
         db.commit()
+    except IntegrityError as e:
+        if isinstance(e.orig, CheckViolation):
+            print(f"user {from_user} cannot follow themselves")
+        if isinstance(e.orig, UniqueViolation):
+            print(f"user {from_user} has already followed {to_user}")
 
 
-def unfollow(db: Session, user: InstagramUser, current_user: InstagramUser) -> None:
+def delete(db: Session, user: InstagramUser, current_user: InstagramUser) -> None:
     follow = (
         db.query(Follow)
         .filter(Follow.from_user_id == current_user.id)
--- instagram/follows/views.py
+++ instagram/follows/views.py
@@ -4,24 +4,24 @@ from instagram.user.models import InstagramUser
 from instagram.user.service import get_authenticated_user, get_current_user
 from sqlalchemy.orm import Session
 
-from .service import follow, unfollow
+from .service import create, delete
 
-router = APIRouter(prefix="/friendships", tags=["friendships"])
+router = APIRouter(prefix="/follows", tags=["follows"])
 
 
-@router.post("/{user_id}/follow")
-def follow_user(
+@router.post("/{user_id}")
+def create_follow(
+    from_user: InstagramUser = Depends(get_authenticated_user),
+    to_user: InstagramUser = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
+    create(db=db, from_user=from_user, to_user=to_user)
+
+
+@router.post("/{user_id}")
+def delete_follow(
     user: InstagramUser = Depends(get_current_user),
     current_user: InstagramUser = Depends(get_authenticated_user),
     db: Session = Depends(get_db),
 ):
-    follow(db=db, user=user, current_user=current_user)
-
-
-@router.post("/{user_id}/unfollow")
-def unfollow_user(
-    user: InstagramUser = Depends(get_current_user),
-    current_user: InstagramUser = Depends(get_authenticated_user),
-    db: Session = Depends(get_db),
-):
-    unfollow(db=db, user=user, current_user=current_user)
+    delete(db=db, user=user, current_user=current_user)
