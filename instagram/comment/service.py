3fcb09041a9c8dfdba5c1ce7a06796e3c6c189ca
Sam Quinn <samquinn20@gmail.com>
2022-04-17 22:17:41 +0100

    authenticate delete endpoint

--- instagram/comment/service.py
+++ instagram/comment/service.py
@@ -10,20 +10,19 @@ def create(
     comment_in: CommentCreate,
     post: Post,
     current_user: InstagramUser,
-):
+) -> None:
     comment = Comment(text=comment_in.text, post_id=post.id, user_id=current_user.id)
 
     db.add(comment)
     db.commit()
 
 
-def delete(db: Session, post_id: int, comment_id: int):
-    comment = (
-        db.query(Comment)
-        .filter(Post.id == post_id)
-        .filter(Comment.id == comment_id)
-        .one_or_none()
-    )
+def get(db: Session, comment_id: int) -> Comment | None:
+    return db.query(Comment).filter(Comment.id == comment_id).one_or_none()
+
+
+def delete(db: Session, comment_id: int) -> None:
+    comment = db.query(Comment).filter(Comment.id == comment_id).one_or_none()
 
     if comment:
         db.delete(comment)
--- instagram/comment/views.py
+++ instagram/comment/views.py
@@ -1,5 +1,9 @@
-from fastapi import Depends
+from asyncio import start_unix_server
+from email.policy import HTTP
+
+from fastapi import Depends, HTTPException, status
 from fastapi.routing import APIRouter
+from instagram.comment.models import Comment
 from instagram.database.core import get_db
 from instagram.post.models import CommentCreate, Post
 from instagram.post.views import get_current_post
@@ -7,9 +11,23 @@ from instagram.user.models import InstagramUser
 from instagram.user.service import get_authenticated_user
 from sqlalchemy.orm import Session
 
-from .service import create, delete
+from .service import create, delete, get
 
 router = APIRouter(prefix="/comments", tags=["comments"])
+
+
+def get_current_comment(
+    comment_id: int,
+    current_user: InstagramUser = Depends(get_authenticated_user),
+    db: Session = Depends(get_db),
+):
+    comment = get(db=db, comment_id=comment_id)
+    if not comment:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Comment with id={comment_id} not found",
+        )
+    return comment
 
 
 @router.post("/{post_id}")
@@ -27,11 +45,13 @@ def create_comment(
     )
 
 
-@router.delete("/{post_id}/{comment_id}")
+@router.delete("/{comment_id}")
 def delete_comment(
-    comment_id: int,
+    comment: Comment = Depends(get_current_comment),
     current_user: InstagramUser = Depends(get_authenticated_user),
-    current_post: Post = Depends(get_current_post),
     db: Session = Depends(get_db),
 ):
-    delete(db=db, post_id=current_post.id, comment_id=comment_id)
+    if current_user.id != comment.user_id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    delete(db=db, comment_id=comment.id)
